7e216d9a55b73a948397f47e8c971ed4d1e9cc25
shubh batra <godwithsam@gmail.com>
2023-04-13 23:05:04 +0100

    Added some shop related changes.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './components/routes/home/home.component';
 import Navigation from './components/routes/navigation/navigation';
 import Authentication from './components/routes/authentication/authentication.component';
+import Shop from './components/routes/shop/shop.component';
 
-
-const Shop = () => {
-  return  <h1>Iam shop section</h1>;
-}
 
 const App = () => {
 
