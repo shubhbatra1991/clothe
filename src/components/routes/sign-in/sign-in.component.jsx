ee0f86076ed7775fc6f62ca76818dcaa31b3561f
shubh batra <godwithsam@gmail.com>
2023-04-13 15:47:51 +0100

    Added files2

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,28 @@
-const SignIn = () => {
-    return (
-        <div>
-            <h1>sign in</h1>
-        </div>
-    );
-};
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+    signInWithGoogleRedirect,
+    auth
+  } from '../../../utils/firebase/firebase.utils.js';
+import SignUpForm from '../../sign-up/sign-up-form.component.jsx';
 
-export default SignIn;
+  
+  const SignIn = () => {
+    const logGoogleUser = async () => {
+      const { user } = await signInWithGooglePopup();
+      const userDocRef = await createUserDocumentFromAuth(user);
+    };
+
+  
+    return (
+      <div>
+        <h1>Sign In Page</h1>
+        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+        <SignUpForm />
+        
+        <button> Sign out button </button>
+      </div>
+    );
+  };
+  
+  export default SignIn;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,26 +1,68 @@
 import { initializeApp } from 'firebase/app';
-import { 
-    getAuth, 
-    signInWithRedirect, 
-    signInWithPopup, 
-    GoogleAuthProvider
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword
 } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC_fud_yCjxMn9h5BdZOjNG53MKMnnTSsQ",
-    authDomain: "clothing-db-4bbd3.firebaseapp.com",
-    projectId: "clothing-db-4bbd3",
-    storageBucket: "clothing-db-4bbd3.appspot.com",
-    messagingSenderId: "972183047493",
-    appId: "1:972183047493:web:885ca70a1bbce1b878c22c"
+    apiKey: "AIzaSyBboPBoNrrSVw3ghnr0qt58dhtNiVkRF4Y",
+    authDomain: "my-clothing-app-37a38.firebaseapp.com",
+    projectId: "my-clothing-app-37a38",
+    storageBucket: "my-clothing-app-37a38.appspot.com",
+    messagingSenderId: "788873624359",
+    appId: "1:788873624359:web:ade27a76739808081c0702"
   };
+
   
   // Initialize Firebase
   const firebaseApp = initializeApp(firebaseConfig);
 
-
 const provider = new GoogleAuthProvider();
+
 provider.setCustomParameters({
-    prompt: "select_account"
+  prompt: 'select_account',
 });
 
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+
+  if (!email || !password)return;
+
+  return await createAuthUserWithEmailAndPassword(auth,email, password);
+
+};
