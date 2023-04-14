88ad764e6b1bd7ef216b5b45dc91f54efbe88acd
shubh batra <godwithsam@gmail.com>
2023-04-14 19:45:13 +0100

    Completed category-page.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,5 +1,6 @@
-import CategoryItem from '../category-item/category-item.component';
+import DirectoryItem from '../directory-item/directory-item.component';
 
+import './directory.styles.scss';
 
 const Directory = ({categories}) => {
     return (
@@ -8,7 +9,7 @@ const Directory = ({categories}) => {
           {/* Product 1 */}
           {categories.map((category)=> 
             (
-              <CategoryItem key={category.id} category={category} />
+              <DirectoryItem key={category.id} category={category} />
             ))}
         </div>
           
