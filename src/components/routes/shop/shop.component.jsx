68787bd72b777122313b1254092472f52b71dd8f
shubh batra <godwithsam@gmail.com>
2023-04-14 19:20:02 +0100

    Created category page.

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,22 +1,18 @@
-import { useContext, Fragment } from 'react';
-import { CategoryPreview } from '../../category-preview/category-preview.component';
-import { CategoriesContext } from '../../../contexts/categories.context';
+import { Routes, Route } from 'react-router-dom';
+
+import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 
 import './shop.styles.scss';
 
 
-const Shop = () => {
-    const { categoriesMap }  = useContext(CategoriesContext);
 
+const Shop = () => {
     return(
-        <div className='shop-container'>
-            { 
-                Object.keys(categoriesMap).map(title => {
-                    const products = categoriesMap[title];
-                    return <CategoryPreview key={title} products={products} title={title} />
-                }) 
-            }
-        </div>
+        <Routes>
+            <Route index element={<CategoriesPreview />} />
+            <Route path=":category" element={<Category />} />
+        </Routes>
         
     );
 };
